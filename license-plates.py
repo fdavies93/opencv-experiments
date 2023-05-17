6a673f4b5d40183b820ee7c8ff53dd2087b55b79
Frank Davies <fdavies93@gmail.com>
2023-05-17 20:27:34 +0800

    Updated license plate code to find multiple matches and use techniques to improve the accuracy.

--- license-plates.py
+++ license-plates.py
@@ -23,10 +23,15 @@ hawaii_9_bw = cv2.cvtColor(hawaii_9, cv2.COLOR_BGR2GRAY)
 # methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
 #             'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
 
+# use the whole image because we're finding multiple matches
+img_copy = master_image.copy()
 # take the 2 right-hand columns of the image ONLY (because we used the left for the source)
-img_copy = master_image.copy()[: , template_0_x + template_0_w : ]
+# img_copy = master_image.copy()[: , template_0_x + template_0_w : ]
 # make it black and white
 img_copy = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+# adjust the black and white template for matching
+# alpha is relative contrast, beta is absolute brightness
+img_copy = cv2.convertScaleAbs(img_copy, alpha=3.5, beta=-150.0)
 
 # get matching spaces for the 0 and the 9 template using the TM_CCOEFF method (whatever this is)
 res_0 = cv2.matchTemplate(img_copy, hawaii_0_bw, cv2.TM_CCOEFF)
@@ -36,18 +41,27 @@ res_9 = cv2.matchTemplate(img_copy, hawaii_9_bw, cv2.TM_CCOEFF)
 for res in [res_0, res_9]:
     # find the location of the best match
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    top_left = max_loc
-    bottom_right = (top_left[0] + template_w, top_left[1] + template_h)
 
-    # draw a white rectangle on the source image
-    cv2.rectangle(img_copy, top_left, bottom_right, 255, 2)
+    # calculate a cutoff point based on the closest match for any valid matches
+    max_thresh = (max_val + 1e-6) * 0.55
+    # cut off the matches array based on threshold
+    match_locations = np.where(res>=max_thresh)
+    
+    # draw a square for each match meeting the threshold
+    for (x, y) in zip(match_locations[1], match_locations[0]):
+        cv2.rectangle(master_image, (x, y), (x + template_w, y + template_h), 255, 2)
+    # top_left = max_loc
+    # bottom_right = (top_left[0] + template_w, top_left[1] + template_h)
+
+    # # draw a white rectangle on the source image
+    # cv2.rectangle(img_copy, top_left, bottom_right, 255, 2)
 
 # make a window called "License Plate"
 cv2.namedWindow("License Plate")
 
 while True:
     # show the image in the window
-    cv2.imshow("License Plate", img_copy)
+    cv2.imshow("License Plate", master_image)
     k = cv2.waitKey(int(1000 / frames_per_second))
     # if we pressed ESCAPE, exit the program
     if k == 27:
